11bb176d7d6e8d474fc64459d5a27a4ac6a2cc05
patiludayk <patiludayk@gmail.com>
2022-11-20 23:12:46 +0000

    Initial commit

---
+++ src/test/java/com/uday/facebook/FacebookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday.facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
